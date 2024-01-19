69895710ecdae39aec3380a6af06d4fc43287a2e
MeiJu <almj1018@gmail.com>
2024-01-20 00:06:59 +0800

    9 - add new other settings window

---
+++ Tchoukball_Scoreboard_MJ/Tchoukball_Scoreboard_MJ/View/OtherSettingsWindowView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tchoukball_Scoreboard_MJ.View
+{
+    /// <summary>
+    /// Interaction logic for OtherSettingsWindowView.xaml
+    /// </summary>
+    public partial class OtherSettingsWindowView : Window
+    {
+        public OtherSettingsWindowView()
+        {
+            InitializeComponent();
+        }
+    }
+}
