b25b3ebc7e5973463b0ad37b7ccb0b7c6b2c6509
MeiJu <almj1018@gmail.com>
2024-01-21 00:30:21 +0800

    9 - added timespan to text converter

---
+++ Tchoukball_Scoreboard_MJ/Tchoukball_Scoreboard_MJ/Converter/TimeSpanToTextConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Tchoukball_Scoreboard_MJ.Converter;
+
+public class TimeSpanToTextConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        TimeSpan ts = (TimeSpan)value;
+        return ts.ToString("m\\:ss");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        TimeSpan.TryParseExact(value.ToString(), "m\\:ss", CultureInfo.InvariantCulture, out var ts);
+        return ts;
+    }
+}
